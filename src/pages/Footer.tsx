d80a9e1cc31910a9eb8414d900539431203466d1
Ahmad Dzulfikar Ubaidillah <dzulfikarubaid@gmail.com>
2023-08-09 16:50:02 +0700

    initial commit

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 
-function Footer() {
+function Footer(props:any) {
+  const{className} = props
   return (
-    <>
+    <div className={className}>
     <div className='text-center bg-blue-400 w-full h-10'></div>
-    <div className='text-center bg-blue-500 w-full p-10 text-white'>
+    <div className='text-center bg-blue-500 w-full p-10  text-white'>
         <h1>
             Sekretariat Dewan Pengurus Pusat<br/>
             Jalan Simpang Tiga Kalibata No. 01.A<br/>
             Duren Tiga - Jakarta Selatan 12830 - INDONESIA<br/>
             Telepon [62-21] 2208-3446<br/>
             Email : dpp@aeli.or.id, Milis AELI : aeli_indonesia@yahoo.com
-</h1>
+        </h1>
     </div>
-    </>
+    </div>
     
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,21 @@
 import '@/styles/globals.css'
-
 import type { AppProps } from 'next/app'
 import Navbar from './components/Dropdown'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import Headroom from 'react-headroom'
 import "@/styles/leaflet.css";
 import Footer from './Footer'
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      
-        <Navbar></Navbar>
-
-      <div className='pb-20 pt-[88px]'>
-      <Component {...pageProps} />
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <div>
+        <Navbar />
       </div>
-      
-      <Footer></Footer>
-      
+      <div style={{ flex: 1, paddingBottom: '20px', paddingTop: '88px' }}>
+        <Component {...pageProps} />
+      </div>
+      <Footer />
     </div>
-      
-    
-  ) 
+  );
 }
