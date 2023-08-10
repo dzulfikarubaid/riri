880c2e7e88ca64ed446800f846e7f8e713532cee
Ahmad Dzulfikar Ubaidillah <dzulfikarubaid@gmail.com>
2023-08-10 21:46:09 +0700

    update menu

--- src/pages/aeli/index.tsx
+++ src/pages/aeli/index.tsx
@@ -13,12 +13,16 @@ function index() {
     <Navbar>
       <div className='w-full'>
       <Title id='aeli'>Asosiasi Experiental Learning Indonesia</Title>
-        <p className='pt-48'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis quas nam impedit quam rerum, perferendis eligendi aliquid id quod consequatur cumque aspernatur, ut repellendus! Magni tenetur illum officiis consequuntur eius.</p>
-        <p className='pt-48'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius alias ut molestiae minima quaerat, dolorum perferendis consectetur quod ab illo laboriosam assumenda blanditiis dignissimos cupiditate distinctio saepe non quis ullam.</p>
+        <p className='pt-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis quas nam impedit quam rerum, perferendis eligendi aliquid id quod consequatur cumque aspernatur, ut repellendus! Magni tenetur illum officiis consequuntur eius.</p>
+        
         <Title id='visi-misi'>Visi Misi</Title>
+        <p className='pt-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius alias ut molestiae minima quaerat, dolorum perferendis consectetur quod ab illo laboriosam assumenda blanditiis dignissimos cupiditate distinctio saepe non quis ullam.</p>
         <Title id='sejarah'>Sejarah</Title>
+        <p className='pt-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius alias ut molestiae minima quaerat, dolorum perferendis consectetur quod ab illo laboriosam assumenda blanditiis dignissimos cupiditate distinctio saepe non quis ullam.</p>
         <Title id='program'>Program</Title>
+        <p className='pt-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius alias ut molestiae minima quaerat, dolorum perferendis consectetur quod ab illo laboriosam assumenda blanditiis dignissimos cupiditate distinctio saepe non quis ullam.</p>
         <Title id='sertifikasi'>Sertifikasi</Title>
+        <p className='pt-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius alias ut molestiae minima quaerat, dolorum perferendis consectetur quod ab illo laboriosam assumenda blanditiis dignissimos cupiditate distinctio saepe non quis ullam.</p>
       </div>
         
     </Navbar>
