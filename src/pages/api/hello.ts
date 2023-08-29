0e2a5ce5a3132eb5072249d28d2b8f4ead9c8242
Ahmad Dzulfikar Ubaidillah <dzulfikarubaid@gmail.com>
2023-08-29 17:07:42 +0700

    update

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,20 +1,16 @@
-// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import type { NextApiRequest, NextApiResponse } from 'next'
-// import { retrieveData } from '@/lib/firebase/service'
-// type Data = {
-//   name: string,
-//   email: string,
-//   message:string,
-// }
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { retrieveData } from '@/lib/firebase/service'
+type Data = {
+  data:any,
+  status:boolean,
+  statusCode:number,
+}
 
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<Data>
-// ) {
-//   try {
-//     const data = await retrieveData('users') // Make sure retrieveData is asynchronous
-//     res.status(200).json(data) // Sending the retrieved data directly
-//   } catch (error) {
-//     res.status(500).json({ message: 'Internal Server Error' })
-//   }
-// }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const data = await retrieveData("users")
+  res.status(200).json({ data, status: true, statusCode: 200 })
+}
