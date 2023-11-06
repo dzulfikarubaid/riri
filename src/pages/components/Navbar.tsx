76ede3cf57940b8e518716fece5ed3f769522930
Ahmad Dzulfikar Ubaidillah <dzulfikarubaid@gmail.com>
2023-11-07 00:43:06 +0700

    update

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,50 +1,61 @@
-
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
-import { BiChevronDown, BiUser, BiUserCircle } from "react-icons/bi";
-import {signIn, signOut, useSession} from 'next-auth/react'
-import Image from 'next/image';
+import { signIn, signOut, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
-import { FaRegUserCircle, FaUserCircle } from 'react-icons/fa';
-import { FaCircleUser, FaRegCircleUser } from 'react-icons/fa6';
-import {PiUserCircleLight} from 'react-icons/pi'
-function Navbar(props:any){
-  const {className} = props
-  const {data}:any = useSession()
+import { PiUserCircleLight } from 'react-icons/pi';
+
+// Define a type for user data
+type UserData = {
+  name?: string | null | undefined;
+  email?: string | null | undefined;
+  image?: string | null | undefined;
+};
+
+function Navbar(props: any) {
+  const { className } = props;
+  const { data, status } = useSession();
+  const { push } = useRouter();
   
-  const {push} = useRouter()
-  function handleSignout(){
-    signOut()
+  // Use the defined type for user data
+  const [user, setUser] = useState<UserData | null>(null);
+
+  function handleSignout() {
+    signOut();
   }
-    return(
-      <div className=''>
-        
-        <div className={`${className}  flex justify-between flex-row w-full text-black  px-24  items-center `} >
-            <Link href={'/'} className='font-extrabold text-2xl text-blue-500'>RIRI<span className='text-black'>.</span></Link>
-            <div className={`flex flex-row gap-8 items-center text-black h-24`}>
-              <Link className='hover:border-b-4 border-black' href={'/'}>Home</Link>
-              <Link className='hover:border-b-4 border-black'  href="/dashboard">Dashboard</Link>
-              <Link className='hover:border-b-4 border-black'  href="/service">Services</Link>
-              <Link className=' hover:border-b-4 border-black'  href="/about">About Us</Link>
-            </div>
-            
-            {/* <li><Link href="/signin" className={` py-2 px-3 ${!white ? 'text-black bg-white hover:bg-gray-100' : 'text-white bg-blue-500 hover:bg-blue-600'}`}>Sign In</Link></li> */}
-            <div>
-            {
-              data ?
-              <div className='flex flex-row gap-4 items-center'>
-                <Link className={`text-black`} href={`/profile`}>{data.user.name}</Link>
-                <button className={` py-2 px-3 bg-blue-500 text-white rounded-full`} onClick={handleSignout}>Sign Out</button>
-              </div>
-              :
-              <button onClick={()=>signIn()}><PiUserCircleLight size={25}/></button>
-            }
-            </div>
+
+  useEffect(() => {
+    // Set the user state when the session changes
+    if (status === 'authenticated') {
+      // Use the optional chaining operator to safely access nested properties
+      setUser(data?.user || null);
+    } else {
+      setUser(null);
+    }
+  }, [data, status]);
+
+  return (
+    <div>
+      <div className={`${className}  flex justify-between flex-row w-full text-black  px-24  items-center `} >
+        <Link href={'/'} className='font-extrabold text-2xl text-blue-500'>RIRI<span className='text-black'>.</span></Link>
+        <div className={`flex flex-row gap-8 items-center text-black h-24`}>
+          <Link className='hover:border-b-4 border-black' href={'/'}>Home</Link>
+          <Link className='hover:border-b-4 border-black'  href="/dashboard">Dashboard</Link>
+          <Link className='hover:border-b-4 border-black'  href="/service">Services</Link>
+          <Link className='hover:border-b-4 border-black'  href="/about">About Us</Link>
         </div>
-        
+        <div>
+          {user ? (
+            <div className='flex flex-row gap-4 items-center'>
+              <Link className={`text-black`} href={`/profile`}>{user.name}</Link>
+              <button className={`py-2 px-3 bg-blue-500 text-white rounded-full`} onClick={handleSignout}>Sign Out</button>
+            </div>
+          ) : (
+            <button onClick={() => signIn()}><PiUserCircleLight size={25}/></button>
+          )}
+        </div>
       </div>
-        
-        
-    )
+    </div>
+  );
 }
+
 export default Navbar;
