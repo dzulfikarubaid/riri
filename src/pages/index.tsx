40d3b6a44a31e6791c132f51a66ae0fb3727e5f8
Ahmad Dzulfikar Ubaidillah <dzulfikarubaid@gmail.com>
2023-11-07 01:05:23 +0700

    u

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { useSession } from 'next-auth/react'
 
 function Index() {
+  const {data} = useSession()
   return (
     <div>
         <Navbar></Navbar>
+        <h1>Signed as {data?.user?.name || ''}</h1>
         <div className='flex flex-col px-24 pt-10 justify-center'>
         <div className='flex flex-row gap-10 justify-center'>
         <h1 className='text-[50px] font-extrabold '>Indonesia&apos;s leading baby anthropometry service using image processing and artificial intelligence.</h1>
