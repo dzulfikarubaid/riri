673e04540490663587566c75d7e62492c92b8d6e
dzulfikarubaid <macbook@MacBooks-MacBook-Pro.local>
2023-11-10 16:04:07 +0700

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <title>RIRI ITS</title>
-      <link rel="shortcut icon" href="riri-logo.png" type="image/x-icon" />
+      <link rel="shortcut icon" href="/riri-logo.png" type="image/x-icon" />
       <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet"></link>
       <body className='font-sans'>
         <Main/>
